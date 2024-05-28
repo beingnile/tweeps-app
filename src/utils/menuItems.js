d2787671148cdebf17070b365a3c970473ac3392
Nile Okomo <okomonile3@gmail.com>
2024-05-28 18:23:13 +0300

    Rmove menu items for v1.0

--- src/utils/menuItems.js
+++ src/utils/menuItems.js
@@ -1,34 +1,3 @@
-const menuItems = [
-  {
-    id: 1,
-    name: 'home',
-    href: '#home',
-    active: true
-  },
-  {
-    id: 2,
-    name: 'dishes',
-    href: '#dishes',
-    active: false
-  },
-  {
-    id: 3,
-    name: 'about',
-    href: '#about',
-    active: false
-  },
-  {
-    id: 4,
-    name: 'menu',
-    href: '#menu',
-    active: false
-  },
-  {
-    id: 5,
-    name: 'reviews',
-    href: '#reviews',
-    active: false
-  }
-];
+const menuItems = [];
 
 export default menuItems;
