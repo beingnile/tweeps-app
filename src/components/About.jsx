3e69765c1a70b565d48c97e39ce07cc389e89deb
Nile Okomo <okomonile3@gmail.com>
2024-06-06 23:57:05 +0300

    Update About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,10 +14,16 @@ function About () {
         <div className={styles.about__content}>
           <h3 className={styles.about__title}>best food in the area</h3>
           <p className={styles.about__description}>
-            At Tweeps, you get top notch snacks and dishes at sweet prices.
+            Food is what brings people together. At Tweeps, we ensure top-notch customer service and also get to know each other not only as customers but also as a network, building a sense of family.
           </p>
           <p className={styles.about__description}>
-            Your satisfaction is our priority.
+            Our focus is on creating good moments. At Tweeps™ fast food kitchen, we achieve this by offering a variety of nutritious meals at affordable prices. We infuse an African touch into our recipes, menu, and spices, embracing a natural feeling in our delicacies.
+          </p>
+          <p className={styles.about__description}>
+	  		Surrounded by a serene environment filled with people of great diversity and culture, we aim to make you feel at home, even when you’re far away from home.
+          </p>
+          <p className={styles.about__description}>
+	  		So, have you been served yet? ‘Usisonge hapo!’
           </p>
           <a href='https://www.instagram.com/tweeps_fastfood' className='button'>
             learn more
