0e113759604b32a5d2eadd7469f0407447338558
Nile Okomo <okomonile3@gmail.com>
2024-06-06 23:43:35 +0300

    Update Menu header

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,7 +6,7 @@ function Menu () {
   return (
     <section className={styles.menu} id='menu'>
       <h3 className='sub-heading'>Our menu</h3>
-      <h1 className='heading'>Today&apos;s Specials</h1>
+      <h1 className='heading'>Popular dishes for you</h1>
       <div className={styles.menu__container}>
         {menuDishList.map((menu, index) => {
           return <Dish key={index} {...menu} />;
